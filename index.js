ca329126161ff7fc07bb80b188fc1f7ba676b3c8
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-14 18:30:24 +0300

    commit tree 702b9ade0d5d038cf1e19b94d6b037346ddd0235

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(drivers, stringName){
+     
+     const matchingDrivers = drivers.filter(function(name){
+        return name.toUpperCase() == stringName.toUpperCase();
+     });
+     return matchingDrivers;
+    
+}
+const drivers = [];
+drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+
+function fuzzyMatch(drivers, stringLetters){
+    const newDriverList = drivers.filter(function(name){
+        return name.startsWith(stringLetters);
+    });
+    return newDriverList;
+}
+
+function matchName(drivers, stringName){
+    const matchingNames = drivers.filter(function( driver){      
+            return driver.name == stringName;    
+    });
+    return matchingNames;
+}
